663ce233850cf09119eddbb21272450bb80a5fcd
breadbread1984 <breadbread1984@163.com>
2021-10-21 20:56:25 +0800

    update

--- models.py
+++ models.py
@@ -36,6 +36,33 @@ def Unit(in_channels = 64, out_channels = 16, is_last = False):
   results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, tf.shape(x[1])[1], tf.shape(x[1])[2], tf.shape(x[1])[3], x[0].shape[-1])))([block3_results, inputs])
   return tf.keras.Model(inputs = inputs, outputs = results);
 
+def Fusion(in_channels, out_channels, is_last = False):
+  inputs = tf.keras.Input((None, None, None, in_channels)); # inputs.shape = (batch, length, height, width, channels)
+  results = inputs;
+  if is_last == True:
+    results = tf.keras.layers.BatchNormalization()(results);
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv3D(in_channels, kernel_size = (1,1,1), padding = 'valid')(results);
+  results = tf.keras.layers.BatchNormalization()(results);
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv3D(out_channels, kernel_size = (3,3,3), padding = 'same')(results);
+  if is_last == True:
+    sliced_inputs = tf.keras.layers.Lambda(lambda x: x[:,1:-1,...])(inputs); # results.shape = (batch, length - 1, height, width, channels)
+  else:
+    sliced_inputs = inputs; # results.shape = (batch, length, height, width, channels)
+  block1_results = tf.keras.layers.Concatenate(axis = -1)([sliced_inputs, results]);
+  results = tf.keras.layers.BatchNormalization()(block1_results);
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv3D(in_channels + out_channels, kernel_size = (1,1,1), padding = 'valid')(results);
+  results = tf.keras.layers.BatchNormalization()(results);
+  results = tf.keras.layers.ReLU()(results);
+  if is_last == True:
+    results = tf.keras.layers.Conv3D(out_channels, kernel_size = (1,3,3), padding = 'same')(results);
+  else:
+    results = tf.keras.layers.Conv3D(out_channels, kernel_size = (3,3,3), padding = 'same')(results);
+  block2_results = tf.keras.layers.Concatenate(axis = -1)([block1_results, results]);
+  return tf.keras.Model(inputs = inputs, outputs = block2_results);
+
 def TGA():
   inputs = tf.keras.Input((None, None, None, 3)); # inputs.shape = (batch, length, height, width, channels)
   reshaped = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[-3], tf.shape(x)[-2], x.shape[-1])))(inputs); # reshaped.shape = (batch * length, height, width, channels)
@@ -60,7 +87,8 @@ def TGA():
   results = Unit(64 + 16 * 9, 16)(results);
   results = Unit(64 + 16 * 12, 16)(results);
   results = Unit(64 + 16 * 15, 16, is_last = True)(results);
-  
+  results = Fusion(64 + 16 * 18, 16)(results);
+  results = Fusion(64 + 16 * 20, 16, is_last = True)(results);
   return tf.keras.Model(inputs = inputs, outputs = results);
 
 if __name__ == "__main__":
