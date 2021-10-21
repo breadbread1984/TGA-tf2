ad37859b896ab45dfe294bd4043a958df5c6a219
breadbread1984 <breadbread1984@163.com>
2021-10-21 20:23:29 +0800

    update

--- models.py
+++ models.py
@@ -27,6 +27,9 @@ def Unit(in_channels = 64, out_channels = 16, is_last = False):
   results = tf.keras.layers.Conv2D(out_channels, kernel_size = (3,3), padding = 'same')(results); # results.shape = (batch * length, height, width, 16)
   if is_last == True:
     attention = tf.keras.layers.Lambda(lambda x: x[...,-1:])(results); # attention.shape = (batch * length, height, width, 1)
+    attention = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, tf.shape(x[1])[1], tf.shape(x[0])[1], tf.shape(x[0])[2], 1)))([attention, inputs]); # attention.shape = (batch, length, height, width, 1)
+    attention = tf.keras.layers.Lambda(lambda x: tf.nn.softmax(x, axis = 1))(attention); # attention.shape = (batch, length, height, width, 1)
+    attention = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[-3], tf.shape(x)[-2], 1)))(attention); # attention.shape = (batch * length, height, width, 1)
     features = tf.keras.layers.Lambda(lambda x: x[...,:-1])(results); # features.shape = (batch * length, height, width, 15)
     results = tf.keras.layers.Lambda(lambda x: x[0] * x[1])([attention, features]); # results.shape = (batch * length, height, width, 15)
   block3_results = tf.keras.layers.Concatenate(axis = -1)([block2_results, results]); # block3_results.shape = (batch * length, height, width, 64 + 16 + 16 + 16)
@@ -37,6 +40,7 @@ def TGA():
   inputs = tf.keras.Input((None, None, None, 3)); # inputs.shape = (batch, length, height, width, channels)
   reshaped = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[-3], tf.shape(x)[-2], x.shape[-1])))(inputs); # reshaped.shape = (batch * length, height, width, channels)
   # 1) Intra-group Fusion
+  # first part (2d unit x 3)
   results = tf.keras.layers.Conv2D(64, kernel_size = (3,3), padding = 'same')(reshaped);
   results = tf.keras.layers.BatchNormalization()(results);
   results = tf.keras.layers.ReLU()(results);
@@ -46,9 +50,10 @@ def TGA():
   results = tf.keras.layers.Conv2D(64, kernel_size = (3,3), padding = 'same')(results);
   results = tf.keras.layers.BatchNormalization()(results);
   results = tf.keras.layers.ReLU()(results);
-  # 2) temporal feature fusion
+  # second part (3d unit x 1)
   results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, tf.shape(x[1])[1], tf.shape(x[1])[2], tf.shape(x[1])[3], x[0].shape[-1])))([results, inputs]); # results.shape = (batch, length, height, width, 64)
   results = tf.keras.layers.Conv3D(64, kernel_size = (3,3,3), strides = (3,1,1), padding = 'same')(results); # results.shape = (batch, length, height, width, 64)
+  # 2) Inter-group Fusion
   results = Unit(64, 16)(results); # results.shape = (batch, length, height, width, 64 + 16 * 3)
   results = Unit(64 + 16 * 3, 16)(results);
   results = Unit(64 + 16 * 6, 16)(results);
