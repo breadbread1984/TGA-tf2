b07f280aba9fab229abc0641d8bd33c5e029ee8d
breadbread1984 <breadbread1984@163.com>
2021-10-25 14:31:20 +0800

    update

--- models.py
+++ models.py
@@ -24,14 +24,16 @@ def Unit(in_channels = 64, out_channels = 16, is_last = False):
   results = tf.keras.layers.Conv2D(in_channels + out_channels * 2, kernel_size = (1,1), padding = 'valid')(results);
   results = tf.keras.layers.BatchNormalization()(results);
   results = tf.keras.layers.ReLU()(results);
-  results = tf.keras.layers.Conv2D(out_channels, kernel_size = (3,3), padding = 'same')(results); # results.shape = (batch * length, height, width, 16)
   if is_last == True:
+    results = tf.keras.layers.Conv2D(out_channels + 1, kernel_size = (3,3), padding = 'same')(results); # results.shape = (batch * length, height, width, 17)
     attention = tf.keras.layers.Lambda(lambda x: x[...,-1:])(results); # attention.shape = (batch * length, height, width, 1)
     attention = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, tf.shape(x[1])[1], tf.shape(x[0])[1], tf.shape(x[0])[2], 1)))([attention, inputs]); # attention.shape = (batch, length, height, width, 1)
     attention = tf.keras.layers.Lambda(lambda x: tf.nn.softmax(x, axis = 1))(attention); # attention.shape = (batch, length, height, width, 1)
     attention = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[-3], tf.shape(x)[-2], 1)))(attention); # attention.shape = (batch * length, height, width, 1)
-    features = tf.keras.layers.Lambda(lambda x: x[...,:-1])(results); # features.shape = (batch * length, height, width, 15)
-    results = tf.keras.layers.Lambda(lambda x: x[0] * x[1])([attention, features]); # results.shape = (batch * length, height, width, 15)
+    features = tf.keras.layers.Lambda(lambda x: x[...,:-1])(results); # features.shape = (batch * length, height, width, 16)
+    results = tf.keras.layers.Lambda(lambda x: x[0] * x[1])([attention, features]); # results.shape = (batch * length, height, width, 16)
+  else:
+    results = tf.keras.layers.Conv2D(out_channels, kernel_size = (3,3), padding = 'same')(results); # results.shape = (batch * length, height, width, 16)
   block3_results = tf.keras.layers.Concatenate(axis = -1)([block2_results, results]); # block3_results.shape = (batch * length, height, width, 64 + 16 + 16 + 16)
   results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, tf.shape(x[1])[1], tf.shape(x[1])[2], tf.shape(x[1])[3], x[0].shape[-1])))([block3_results, inputs])
   return tf.keras.Model(inputs = inputs, outputs = results);
@@ -47,6 +49,7 @@ def Fusion(in_channels, out_channels, is_last = False):
   results = tf.keras.layers.ReLU()(results);
   results = tf.keras.layers.Conv3D(out_channels, kernel_size = (3,3,3), padding = 'same')(results);
   if is_last == True:
+    results = tf.keras.layers.Lambda(lambda x: x[:,1:-1,...])(results);
     sliced_inputs = tf.keras.layers.Lambda(lambda x: x[:,1:-1,...])(inputs); # results.shape = (batch, length - 1, height, width, channels)
   else:
     sliced_inputs = inputs; # results.shape = (batch, length, height, width, channels)
@@ -63,8 +66,8 @@ def Fusion(in_channels, out_channels, is_last = False):
   block2_results = tf.keras.layers.Concatenate(axis = -1)([block1_results, results]);
   return tf.keras.Model(inputs = inputs, outputs = block2_results);
 
-def TGA():
-  inputs = tf.keras.Input((None, None, None, 3)); # inputs.shape = (batch, length, height, width, channels)
+def TGA(scale = 2):
+  inputs = tf.keras.Input((8, None, None, 3)); # inputs.shape = (batch, length, height, width, channels)
   reshaped = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[-3], tf.shape(x)[-2], x.shape[-1])))(inputs); # reshaped.shape = (batch * length, height, width, channels)
   # 1) Intra-group Fusion
   # first part (2d unit x 3)
@@ -89,12 +92,38 @@ def TGA():
   results = Unit(64 + 16 * 15, 16, is_last = True)(results);
   results = Fusion(64 + 16 * 18, 16)(results);
   results = Fusion(64 + 16 * 20, 16, is_last = True)(results);
+  results = tf.keras.layers.BatchNormalization()(results);
+  results = tf.keras.layers.ReLU()(results);
+  reshaped = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[2], tf.shape(x)[3], x.shape[4])))(results);
+  reshaped = tf.keras.layers.Conv2D(64, kernel_size = (3,3), padding = 'same')(reshaped);
+  results = results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, tf.shape(x[1])[1], tf.shape(x[1])[2], tf.shape(x[1])[3], x[0].shape[-1])))([reshaped, results]);
+  results = tf.keras.layers.BatchNormalization()(results);
+  results = Unit(64, 16)(results);
+  results = Unit(64 + 16 * 3, 16)(results);
+  results = Unit(64 + 16 * 6, 16)(results);
+  results = Unit(64 + 16 * 9, 16)(results);
+  results = Unit(64 + 16 * 12, 16)(results);
+  results = Unit(64 + 16 * 15, 16)(results);
+  results = Unit(64 + 16 * 18, 16)(results);
+  reshaped = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[2], tf.shape(x)[3], x.shape[4])))(results);
+  results = tf.keras.layers.BatchNormalization()(reshaped);
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv2D(256, kernel_size = (3,3), padding = 'same')(results);
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv2D(256, kernel_size = (1,1), padding = 'same')(results);
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv2D(128 * 4, kernel_size = (3,3), padding = 'same')(results); # results = (batch * length = 1, height, width, channels)
+  results = tf.keras.layers.Lambda(lambda x: tf.nn.depth_to_space(x, 2))(results); # results.shape = (batch, height * 2, width * 2, channels / 4)
+  results = tf.keras.layers.ReLU()(results);
+  results = tf.keras.layers.Conv2D(128, kernel_size = (1,1), padding = 'same', activation = tf.keras.activations.relu)(results);
+  results = tf.keras.layers.Conv2D(scale**2 * 3, kernel_size = (1,1), padding = 'same')(results);
+  results = tf.keras.layers.Lambda(lambda x: tf.nn.depth_to_space(x, 2))(results); # results.shape = (batch, height * 2, width * 2, channels / 4)
   return tf.keras.Model(inputs = inputs, outputs = results);
 
 if __name__ == "__main__":
   tga = TGA();
   import numpy as np;
-  inputs = np.random.normal(size = (4,5,10,10,3));
+  inputs = np.random.normal(size = (4,8,10,10,3));
   outputs = tga(inputs);
   print(inputs.shape);
   print(outputs.shape);
